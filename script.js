0ef4661978bfb068802cff7646746d20dbb91270
mattheweq <matthew@olk1.com>
2023-08-24 15:00:02 +0100

    refactor to fetch card content data from json file + add escape key to close card modal + refactor to basic styling

--- script.js
+++ script.js
@@ -29,129 +29,116 @@ const fadeContent = (element, opacity, duration = 300) => {
   })
 }
 
-const getCardContent = (title, type) => {
-  return `
-  <div class="card-content">
-    <h2>${title}</h2>
-    <img src="./assets/${type}.png" alt="${title}">
-    <p>
-      hello/ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor eum ipsa molestiae nesciunt nostrum porro
-      reprehenderit? Animi corporis deleniti dolore laborum, nemo pariatur temporibus voluptatem.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque eligendi fuga ullam? Aperiam blanditiis
-      cupiditate dicta eius exercitationem explicabo fugit, impedit iure libero nam nihil nisi perferendis
-      provident quaerat repellendus vitae voluptate? Aliquid amet architecto asperiores aut consequuntur
-      corporis debitis ea eveniet in maiores, nam placeat quae, ratione rerum ullam?
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor eum ipsa molestiae nesciunt nostrum porro
-      reprehenderit? Animi corporis deleniti dolore laborum, nemo pariatur temporibus voluptatem.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor eum ipsa molestiae nesciunt nostrum porro
-      reprehenderit? Animi corporis deleniti dolore laborum, nemo pariatur temporibus voluptatem.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque eligendi fuga ullam? Aperiam blanditiis
-      cupiditate dicta eius exercitationem explicabo fugit, impedit iure libero nam nihil nisi perferendis
-      provident quaerat repellendus vitae voluptate? Aliquid amet architecto asperiores aut consequuntur
-      corporis debitis ea eveniet in maiores, nam placeat quae, ratione rerum ullam?
-    </p>
-  </div>
-`
-}
+
+const getCardContent = async () => {
+  try {
+    const response = await fetch('content.json'); 
+    const data = await response.json();
+
+    const paragraphs = data.paragraphs.map(paragraph => `<p>${paragraph}</p>`).join('');
+
+    return `
+      <div class="card-content">
+        <h2>${data.title}</h2>
+        <img src="${data.img}" alt="${data.title}">
+        <!-- <img src="images/${data.img}.png" alt="${data.title}"> -->
+        ${paragraphs}
+      </div>
+    `;
+  } catch (error) {
+    console.error('Error fetching content:', error);
+    return ''; // Return an empty string or a default content in case of an error
+  }
+};
+
+
+
+const createCloseButton = () => {
+  const closeButton = document.createElement("button");
+  closeButton.innerHTML = "&#10005";
+  closeButton.style = `
+    position: fixed;
+    z-index: 10000;
+    top: 35px;
+    right: 45px;
+    width: 35px;
+    height: 35px;
+    opacity: 0.1;
+    border: 0;
+    background: transparent;
+    color: white;
+    font-size: 2rem;
+  `;
+  return closeButton;
+};
+
+
+
+const cloneAndPositionCard = (card, top, left, width, height) => {
+  const cardClone = card.cloneNode(true);
+  cardClone.style.position = "fixed";
+  cardClone.style.top = top + "px";
+  cardClone.style.left = left + "px";
+  cardClone.style.width = width + "px";
+  cardClone.style.height = height + "px";
+  return cardClone;
+};
+
+const resetCardStyles = (cardClone) => {
+  cardClone.style.removeProperty("display");
+  cardClone.style.removeProperty("padding");
+  [...cardClone.children].forEach((child) => {
+    child.style.removeProperty("display");
+  });
+};
 
 const onCardClick = async (e) => {
-  const card = e.currentTarget
-  // clone the card
-  const cardClone = card.cloneNode(true)
-  // get the location of the card in the view
-  const { top, left, width, height } = card.getBoundingClientRect()
-  // position the clone on top of the original
-  cardClone.style.position = "fixed"
-  cardClone.style.top = top + "px"
-  cardClone.style.left = left + "px"
-  cardClone.style.width = width + "px"
-  cardClone.style.height = height + "px"
-  // hide the original card with opacity
-  card.style.opacity = "0"
-  // add card to the same container
-  card.parentNode.appendChild(cardClone)
-  // create a close button to handle the undo
-  const closeButton = document.createElement("button")
-  closeButton.innerHTML = "&#10005"
-  // position the close button top corner
-  closeButton.style = `
-  position: fixed;
-  z-index: 10000;
-  top: 35px;
-  right: 45px;
-  width: 35px;
-  height: 35px;
-  opacity: 0.1;
-  border: 0;
-  background: transparent;
-  color: white;
-  font-size: 2rem;
-`
+  const card = e.currentTarget;
+  const { top, left, width, height } = card.getBoundingClientRect();
+  const cardClone = cloneAndPositionCard(card, top, left, width, height);
 
-  // attach click event to the close button
-  closeButton.addEventListener("click", async () => {
-    // remove the button on close
-    closeButton.remove()
-    // remove the display style so the original content is displayed right
-    cardClone.style.removeProperty("display")
-    cardClone.style.removeProperty("padding")
-    // show original card content
-    ;[...cardClone.children].forEach((child) =>
-      child.style.removeProperty("display")
-    )
-    fadeContent(cardClone, "0")
-    // shrink the card back to the original position and size
-    await toggleExpansion(
-      cardClone,
-      {
-        top: `${top}px`,
-        left: `${left}px`,
-        width: `${width}px`,
-        height: `${height}px`
-      },
-      300
-    )
-    // show the original card again
-    card.style.removeProperty("opacity")
-    // remove the clone card
-    cardClone.remove()
-  })
-  // fade the content away
+  card.style.opacity = "0";
+  card.parentNode.appendChild(cardClone);
+
+  const closeButton = createCloseButton();
+  
+  const closeCard = async () => {
+    document.removeEventListener("keydown", handleEscKeyPress);
+    closeButton.remove();
+    resetCardStyles(cardClone);
+    fadeContent(cardClone, "0");
+    await toggleExpansion(cardClone, { top: `${top}px`, left: `${left}px`, width: `${width}px`, height: `${height}px` }, 300);
+    card.style.removeProperty("opacity");
+    cardClone.remove();
+  };
+
+  const handleEscKeyPress = (event) => {
+    if (event.key === "Escape") {
+      closeCard();
+    }
+  };
+
+  document.addEventListener("keydown", handleEscKeyPress);
+
+  closeButton.addEventListener("click", () => {
+    closeCard();
+  });
+
   fadeContent(cardClone, "0").then(() => {
-    ;[...cardClone.children].forEach(
-      (child) => (child.style.display = "none")
-    )
-  })
-  // expand the clone card
-  await toggleExpansion(cardClone, {
-    top: 0,
-    left: 0,
-    width: "100vw",
-    height: "100vh"
-  })
-  const content = getCardContent(card.textContent, card.dataset.type)
-  // set the display block so the content will follow the normal flow in case the original card is not display block
-  cardClone.style.display = "block"
-  cardClone.style.padding = "0"
-  // append the close button after the expansion is done
-  cardClone.appendChild(closeButton)
-  cardClone.insertAdjacentHTML("afterbegin", content)
-}
+    [...cardClone.children].forEach((child) => {
+      child.style.display = "none";
+    });
+  });
 
-cards.forEach((card) => card.addEventListener("click", onCardClick))
+  await toggleExpansion(cardClone, { top: 0, left: 0, width: "100vw", height: "100vh" });
+
+  const content = await getCardContent(card.textContent);
+  cardClone.style.display = "block";
+  cardClone.style.padding = "0";
+  cardClone.appendChild(closeButton);
+  cardClone.insertAdjacentHTML("afterbegin", content);
+};
+
+cards.forEach((card) => card.addEventListener("click", onCardClick));
 
 
-// document.addEventListener("keydown", handleEscKeyPress);
-// function handleEscKeyPress(event) {
-//     if (event.key === "Escape") {
-//       closeNav();
-//     }
-//   }
